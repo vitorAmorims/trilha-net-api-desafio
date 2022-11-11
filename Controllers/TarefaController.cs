442dc9187bc62e3765d375a0deaa9062ed97b920
vitorAmorims <vtamorims@gmail.com>
2022-11-11 11:03:49 -0300

    implementacao GET ObterTodos

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -34,8 +34,7 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
-            return Ok();
+            return Ok(_context.Tarefas.ToList());
         }
 
         [HttpGet("ObterPorTitulo")]
