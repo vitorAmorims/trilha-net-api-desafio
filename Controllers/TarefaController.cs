5c9b1606d5c3166b677293facec2373d35d1b6fe
vitorAmorims <vtamorims@gmail.com>
2022-11-11 11:11:31 -0300

    implementacao POST Criar tarefa

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -68,7 +68,8 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
